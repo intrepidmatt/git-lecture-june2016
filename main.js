1d5107f5ba9f465c3df152a43831cabf378fd124
Matt Bridges <matt@intrepid.io>
2016-06-07 10:17:37 -0400

    Add my favorite candy

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 console.log("Matt's Favorite Things:")
 console.log("* Favorite Beer: Bud Light")
 console.log("* Favorite Movie: Back to the Future")
+console.log("* Favorite Candy: Sour Patch Kids")
 
